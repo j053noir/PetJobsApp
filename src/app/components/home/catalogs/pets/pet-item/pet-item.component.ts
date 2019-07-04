6e5e7868e0950b7c7e23c6d345558027e6540697
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-07-04 18:56:46 -0500

    Added swipe scroll to pets catalog.

--- src/app/components/home/catalogs/pets/pet-item/pet-item.component.ts
+++ src/app/components/home/catalogs/pets/pet-item/pet-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pet-item',
   templateUrl: './pet-item.component.html',
-  styleUrls: ['./pet-item.component.scss']
+  styleUrls: ['./pet-item.component.scss'],
 })
 export class PetItemComponent implements OnInit {
+  @Input() pet: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
