6f3c5a4bf17c44969f0c5f4ec07f7402fbbdd933
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2019-07-05 16:31:12 -0500

    Shop items ready.

--- src/app/components/home/catalogs/shop/shop.component.ts
+++ src/app/components/home/catalogs/shop/shop.component.ts
@@ -6,7 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
+  items = [
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    },
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    },
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    },
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    },
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    },
+    {
+      image: '../../../../../../assets/img/petjobs_logo_header.png',
+      name: 'Clinica Veterinaria Patitas',
+      category: 'Veterinario',
+      workHours: '24 horas',
+      isOpen: true,
+      rating: 4.6,
+      comments: 3,
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
